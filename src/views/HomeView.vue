23f46b726e354120b950f77420cad1e4a96e344d
Edenys Deniz González <holofist1@gmail.com>
2023-07-24 13:17:40 -0400

    init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { useMeta } from 'vue-meta'
+
+useMeta({
+  title: 'asdfasdfdas',
+  htmlAttrs: { lang: 'en', amp: true }
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
