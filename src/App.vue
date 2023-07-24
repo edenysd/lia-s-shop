03a4c1b01559d4d69dfd44777cb6e2c5043a9fee
Edenys Deniz González <holofist1@gmail.com>
2023-07-24 13:32:19 -0400

    feat: add meta title template

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <!-- Needed by vue-meta -->
-  <metainfo />
+  <metainfo>
+    <!-- Title template -->
+    <template v-slot:title="{ content }">{{ content }} | Template </template>
+  </metainfo>
 
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
