6d960fc93c187954d5a7386e5ffa5daada80882e
Edenys Deniz González <holofist1@gmail.com>
2023-11-03 17:44:35 -0400

    feat: connect primevue to vue app

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createMetaManager } from 'vue-meta'
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import Tooltip from 'primevue/tooltip'
 
 import App from './App.vue'
 import router from './router'
+import 'primevue/resources/themes/lara-light-teal/theme.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(createMetaManager())
+app.use(PrimeVue)
+app.use(ToastService)
+app.directive('tooltip', Tooltip)
 
 app.mount('#app')
