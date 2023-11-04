ecb809ee3fbf36c16619817699c568cef1820be0
Edenys Deniz González <holofist1@gmail.com>
2023-11-04 02:27:42 -0400

    feat: add product dialog

---
+++ src/components/ProductDialog/ProductDialogComponent.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { ref } from 'vue'
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import InputNumber from 'primevue/inputnumber'
+import RatingComponent from '../atoms/RatingComponent.vue'
+
+import { useCartStore } from '../../stores/cart'
+
+const props = defineProps(['visible', 'product'])
+const emits = defineEmits(['update:visible'])
+const desiredAmount = ref(0)
+
+const cartStore = useCartStore()
+
+const confirmAddToCart = () => {
+  cartStore.addProduct(props.product.id, desiredAmount)
+  emits('update:visible', false)
+}
+</script>
+
+<template>
+  <Dialog
+    class="w-12 sm:w-10 md:w-8"
+    v-if="props.product"
+    :visible="props.visible"
+    @update:visible="(e) => $emit('update:visible', e)"
+    modal
+    :header="props.product.title"
+  >
+    <div class="grid">
+      <div class="col-12 md:col-6 relative">
+        <img
+          class="overflow-hidden"
+          :src="props.product.image"
+          alt="Image"
+          style="width: 100%; height: 300px; object-fit: contain; z-index: -1"
+        />
+        <div
+          class="w-full h-3rem"
+          style="position: absolute; bottom: 0px; backdrop-filter: blur(10px)"
+        ></div>
+        <RatingComponent
+          style="position: absolute; left: 10px; bottom: 10px"
+          :rate="props.product.rating.rate"
+          :count="props.product.rating.count"
+        />
+        <div
+          class="text-2xl font-semibold text-color-secondary"
+          style="position: absolute; right: 10px; bottom: 10px"
+        >
+          ${{ props.product.price }}
+        </div>
+      </div>
+      <div class="col-12 md:col-6 flex flex-column justify-content-between">
+        <div>
+          {{ props.product.description }}
+        </div>
+        <div class="mt-4">
+          <div class="relative w-full flex justify-content-end align-items-center">
+            <InputNumber
+              input-class="w-5rem"
+              class="mr-2"
+              v-model="desiredAmount"
+              inputId="minmax-buttons"
+              mode="decimal"
+              showButtons
+              :min="0"
+              :max="10000"
+            />
+
+            <Button
+              class="pl-2 pr-2 hover-scale transition-transform animation-duration-100"
+              icon="pi pi-cart-plus"
+              severity="primary"
+              outlined=""
+              @click="confirmAddToCart"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </Dialog>
+</template>
