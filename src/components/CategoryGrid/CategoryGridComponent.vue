ecee13561c726802a5ce4a04022d1fe3b7d26155
Edenys Deniz González <holofist1@gmail.com>
2023-11-04 05:03:20 -0400

    feat: add category grid animations

--- src/components/CategoryGrid/CategoryGridComponent.vue
+++ src/components/CategoryGrid/CategoryGridComponent.vue
@@ -18,7 +18,10 @@ categoryStore.fetchAllCategories()
 </script>
 
 <template>
-  <div class="flex flex-column justify-content-center align-items-center w-full p-2">
+  <div
+    class="flex flex-column justify-content-center align-items-center w-full p-2"
+    style="max-width: 1200px"
+  >
     <div class="pb-5 text-5xl text-color-secondary font-medium">Category List</div>
     <ProgressBar
       v-if="categoryStore.loadingCategories"
@@ -26,20 +29,25 @@ categoryStore.fetchAllCategories()
       class="w-25rem"
       style="height: 6px"
     />
-    <div v-else class="grid w-full">
+    <div v-else class="grid w-full p-8">
       <CategoryItemComponent
         v-for="category of categoryStore.allCategories"
-        class="col-12 sm:col-6"
+        class="col-12 sm:col-6 hover-scale-105 transition-transform transition-duration-100"
         :key="category"
         :name="category.toUpperCase()"
         @click="() => filterByCategory(category)"
       />
       <CategoryItemComponent
         :filled="true"
-        class="col-12"
+        class="col-12 hover-scale-105 transition-transform transition-duration-100"
         name="ALL CATEGORIES"
         @click="() => filterByCategory(null)"
       />
     </div>
   </div>
 </template>
+<style scoped>
+.hover-scale-105:hover {
+  transform: scale(105%, 105%);
+}
+</style>
