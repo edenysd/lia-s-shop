a7850a3925bb2311089727f99128cb8bd2ab059d
Edenys Deniz González <holofist1@gmail.com>
2023-11-04 04:35:39 -0400

    fix: remove request timeout

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiInstance = axios.create({
   baseURL: 'https://fakestoreapi.com/',
-  timeout: 5000,
+  // timeout: 15000,
   headers: {
     'X-Custom-Header': 'foobar',
     'Content-Type': 'application/json'
